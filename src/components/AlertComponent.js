1acf071fb2a4ff566bae3d269b5c3b202ad3ce74
justin-nykiel <justin.nykiel@gmail.com>
2021-07-15 20:42:56 -0500

    cleaned up a comment

--- src/components/AlertComponent.js
+++ src/components/AlertComponent.js
@@ -6,7 +6,6 @@ import '../css/AlertComponent.css'
 
 function AlertComponent({ alert }) {
   const alertDispatch = useContext(DispatchContext)
-  // const component = <Zoom in={true}><Alert onClose={() => {alertDispatch({ type: 'remove', payload: {id:alert.id, alertType:alert.alertType }})}} className={alert.className} severity={alert.alertType || "info"} ><AlertTitle>{alert.alertTitle}</AlertTitle>{alert.text}</Alert></Zoom>
   if(alert.link){
     return (
       <Zoom in={true}><Alert onClose={() => {alertDispatch({ type: 'remove', payload: {id:alert.id, alertType:alert.alertType }})}} className={alert.className} severity={alert.alertType || "info"} ><AlertTitle className={"alert"}><Link target="_blank" rel="nofollow" href={alert.link}>{alert.alertTitle}</Link></AlertTitle>{alert.text}</Alert></Zoom>
